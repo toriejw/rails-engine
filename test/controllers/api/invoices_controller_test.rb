a7c9d4bf6fd1dc7be3a2809880ad455aa33154f6
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-01-05 15:11:49 -0700

    Add tests for find_all method in invoice controller

--- test/controllers/api/invoices_controller_test.rb
+++ test/controllers/api/invoices_controller_test.rb
@@ -24,6 +24,65 @@ class Api::V1::InvoicesControllerTest < ActionController::TestCase
     assert_equal invoice_id, parsed_response["id"]
   end
 
+  test "#find returns invidual record when searched by customer_id" do
+    customer_id = Invoice.first.customer_id
+    get :find, format: :json, customer_id: customer_id
+
+    assert_response :success
+    assert_kind_of Hash, parsed_response
+    assert_equal customer_id, parsed_response["customer_id"]
+  end
+
+  test "#find returns invidual record when searched by merchant_id" do
+    merchant_id = Invoice.first.merchant_id
+    get :find, format: :json, merchant_id: merchant_id
+
+    assert_response :success
+    assert_kind_of Hash, parsed_response
+    assert_equal merchant_id, parsed_response["merchant_id"]
+  end
+
+  test "#find returns invidual record when searched by status" do
+    status = Invoice.first.status
+    get :find, format: :json, status: status
+
+    assert_response :success
+    assert_kind_of Hash, parsed_response
+    assert_equal status, parsed_response["status"]
+  end
+
+  test "#find_all returns array of all records matching search parameter for customer_id" do
+    customer_customer_id = Invoice.first.customer_id
+    get :find_all, format: :json, customer_id: customer_customer_id
+
+    assert_response :success
+    assert_kind_of Array, parsed_response
+    assert_equal Invoice.where(customer_id: customer_customer_id).count, parsed_response.count
+
+    check_response_array_for_correct_data
+  end
+
+  test "#find_all returns array of all records matching search parameter for merchant_id" do
+    customer_merchant_id = Invoice.first.merchant_id
+    get :find_all, format: :json, merchant_id: customer_merchant_id
+
+    assert_response :success
+    assert_kind_of Array, parsed_response
+    assert_equal Invoice.where(merchant_id: customer_merchant_id).count, parsed_response.count
+
+    check_response_array_for_correct_data
+  end
+
+  test "#find_all returns array of all records matching search parameter for status" do
+    customer_status = Invoice.first.status
+    get :find_all, format: :json, status: customer_status
+
+    assert_response :success
+    assert_kind_of Array, parsed_response
+    assert_equal Invoice.where(status: customer_status).count, parsed_response.count
+
+    check_response_array_for_correct_data
+  end
 
   test "#random returns individual record" do
     get :random, format: :json
