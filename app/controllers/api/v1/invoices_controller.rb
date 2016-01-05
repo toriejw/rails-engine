7797f3b9db794b69533a90824f48d176ca6d40fb
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-01-04 20:09:30 -0700

    Add find method to invoices controller

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -8,4 +8,22 @@ class Api::V1::InvoicesController < ApplicationController
   def show
     respond_with Invoice.find(params[:id])
   end
+
+  def find
+    respond_with find_invoice(params)
+  end
+
+  private
+
+    def find_invoice(params)
+      if params[:id]
+        Invoice.find_by(id: params[:id])
+      elsif params[:customer_id]
+        Invoice.find_by(customer_id: params[:customer_id])
+      elsif params[:merchant_id]
+        Invoice.find_by(merchant_id: params[:merchant_id])
+      elsif params[:status]
+        Invoice.find_by(status: params[:status])
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get "/customers/find", to: "customers#find"
       get "/invoice_items/find", to: "invoice_items#find"
+      get "/invoices/find", to: "invoices#find"
 
       resources :customers, only: [:index, :show], defaults: { format: :json }
       resources :merchants, only: [:index, :show], defaults: { format: :json }
