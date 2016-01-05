7cce7f433476a1391cfc205ab338338811e96ce9
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-01-05 15:16:29 -0700

    Add tests for find_all method in transaction controller

--- test/controllers/api/transactions_controller_test.rb
+++ test/controllers/api/transactions_controller_test.rb
@@ -24,6 +24,65 @@ class Api::V1::TransactionsControllerTest < ActionController::TestCase
     assert_equal transaction_id, parsed_response["id"]
   end
 
+  test "#find returns invidual record when searched by invoice_id" do
+    invoice_id = Transaction.first.invoice_id
+    get :find, format: :json, invoice_id: invoice_id
+
+    assert_response :success
+    assert_kind_of Hash, parsed_response
+    assert_equal invoice_id, parsed_response["invoice_id"]
+  end
+
+  test "#find returns invidual record when searched by credit_card_number" do
+    credit_card_number = Transaction.first.credit_card_number
+    get :find, format: :json, credit_card_number: credit_card_number
+
+    assert_response :success
+    assert_kind_of Hash, parsed_response
+    assert_equal credit_card_number, parsed_response["credit_card_number"]
+  end
+
+  test "#find returns invidual record when searched by result" do
+    result = Transaction.first.result
+    get :find, format: :json, result: result
+
+    assert_response :success
+    assert_kind_of Hash, parsed_response
+    assert_equal result, parsed_response["result"]
+  end
+
+  test "#find_all returns array of all records matching search parameter for invoice_id" do
+    customer_invoice_id = Transaction.first.invoice_id
+    get :find_all, format: :json, invoice_id: customer_invoice_id
+
+    assert_response :success
+    assert_kind_of Array, parsed_response
+    assert_equal Transaction.where(invoice_id: customer_invoice_id).count, parsed_response.count
+
+    check_response_array_for_correct_data
+  end
+
+  test "#find_all returns array of all records matching search parameter for credit_card_number" do
+    customer_credit_card_number = Transaction.first.credit_card_number
+    get :find_all, format: :json, credit_card_number: customer_credit_card_number
+
+    assert_response :success
+    assert_kind_of Array, parsed_response
+    assert_equal Transaction.where(credit_card_number: customer_credit_card_number).count, parsed_response.count
+
+    check_response_array_for_correct_data
+  end
+
+  test "#find_all returns array of all records matching search parameter for result" do
+    customer_result = Transaction.first.result
+    get :find_all, format: :json, result: customer_result
+
+    assert_response :success
+    assert_kind_of Array, parsed_response
+    assert_equal Transaction.where(result: customer_result).count, parsed_response.count
+
+    check_response_array_for_correct_data
+  end
 
   test "#random returns individual record" do
     get :random, format: :json
