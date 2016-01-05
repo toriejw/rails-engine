589bfb658ab55b90f6f99f0b83af952bbadb2ffa
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-01-05 15:13:45 -0700

    Add tests for find_all method in item controller

--- test/controllers/api/items_controller_test.rb
+++ test/controllers/api/items_controller_test.rb
@@ -25,6 +25,85 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
     assert_equal item_id, parsed_response["id"]
   end
 
+  test "#find returns invidual record when searched by name" do
+    name = Item.first.name
+    get :find, format: :json, name: name
+
+    assert_response :success
+    assert_kind_of Hash, parsed_response
+    assert_equal name, parsed_response["name"]
+  end
+
+  test "#find returns invidual record when searched by description" do
+    description = Item.first.description
+    get :find, format: :json, description: description
+
+    assert_response :success
+    assert_kind_of Hash, parsed_response
+    assert_equal description, parsed_response["description"]
+  end
+
+  test "#find returns invidual record when searched by unit_price" do
+    unit_price = Item.first.unit_price
+    get :find, format: :json, unit_price: unit_price
+
+    assert_response :success
+    assert_kind_of Hash, parsed_response
+    assert_equal unit_price, parsed_response["unit_price"]
+  end
+
+  test "#find returns invidual record when searched by merchant_id" do
+    merchant_id = Item.first.merchant_id
+    get :find, format: :json, merchant_id: merchant_id
+
+    assert_response :success
+    assert_kind_of Hash, parsed_response
+    assert_equal merchant_id, parsed_response["merchant_id"]
+  end
+
+  test "#find_all returns array of all records matching search parameter for name" do
+    customer_name = Item.first.name
+    get :find_all, format: :json, name: customer_name
+
+    assert_response :success
+    assert_kind_of Array, parsed_response
+    assert_equal Item.where(name: customer_name).count, parsed_response.count
+
+    check_response_array_for_correct_data
+  end
+
+  test "#find_all returns array of all records matching search parameter for description" do
+    customer_description = Item.first.description
+    get :find_all, format: :json, description: customer_description
+
+    assert_response :success
+    assert_kind_of Array, parsed_response
+    assert_equal Item.where(description: customer_description).count, parsed_response.count
+
+    check_response_array_for_correct_data
+  end
+
+  test "#find_all returns array of all records matching search parameter for unit_price" do
+    customer_unit_price = Item.first.unit_price
+    get :find_all, format: :json, unit_price: customer_unit_price
+
+    assert_response :success
+    assert_kind_of Array, parsed_response
+    assert_equal Item.where(unit_price: customer_unit_price).count, parsed_response.count
+
+    check_response_array_for_correct_data
+  end
+
+  test "#find_all returns array of all records matching search parameter for merchant_id" do
+    customer_merchant_id = Item.first.merchant_id
+    get :find_all, format: :json, merchant_id: customer_merchant_id
+
+    assert_response :success
+    assert_kind_of Array, parsed_response
+    assert_equal Item.where(merchant_id: customer_merchant_id).count, parsed_response.count
+
+    check_response_array_for_correct_data
+  end
 
   test "#random returns individual record" do
     get :random, format: :json
