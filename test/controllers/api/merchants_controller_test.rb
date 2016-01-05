3c025f6a7bf78d8aadf5e8443776ba77bd513743
Torie Joy-Warren <victoria.joywarren@gmail.com>
2016-01-05 15:14:53 -0700

    Add tests for find_all method in merchant controller

--- test/controllers/api/merchants_controller_test.rb
+++ test/controllers/api/merchants_controller_test.rb
@@ -22,6 +22,25 @@ class Api::V1::MerchantsControllerTest < ActionController::TestCase
     assert_equal merchant_id, parsed_response["id"]
   end
 
+  test "#find returns invidual record when searched by name" do
+    name = Merchant.first.name
+    get :find, format: :json, name: name
+
+    assert_response :success
+    assert_kind_of Hash, parsed_response
+    assert_equal name, parsed_response["name"]
+  end
+
+  test "#find_all returns array of all records matching search parameter for name" do
+    customer_name = Merchant.first.name
+    get :find_all, format: :json, name: customer_name
+
+    assert_response :success
+    assert_kind_of Array, parsed_response
+    assert_equal Merchant.where(name: customer_name).count, parsed_response.count
+
+    check_response_array_for_correct_data
+  end
 
   test "#random returns individual record" do
     get :random, format: :json
